ccf6e574711086a87c356d3e57c41a69f31fcf07
Washington Nieto <washington_nieto@hotmail.com>
2025-01-31 11:56:10 -0500

    Fin Proyecto Portafolio

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ListadoTrabajos } from './ListadosTrabajos'
 
 export const Inicio = () => {
   return (
@@ -10,7 +11,7 @@ export const Inicio = () => {
         y ofrezco mis servicios de <strong>programación</strong> en todo 
         tipo de proyectos web.
       </h1>
-      <h2>
+      <h2 className='title'>
         Te ayudo a crea tu sitio o aplicación web, tener más visibilidad
         y relevancia en internet. <Link to="/contacto">Contacta conmigo.</Link>
       </h2>
@@ -19,9 +20,7 @@ export const Inicio = () => {
         <h2 className='heading'>Algunos de mis proyectos</h2>
         <p>Estos son algunos de mis trabajos desarrollados</p>
 
-        <div className='works'>
-
-        </div>
+        <ListadoTrabajos limite="2"/>
 
       </section>
     </div>
